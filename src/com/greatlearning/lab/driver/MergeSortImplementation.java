b1506cab3cf329f05b091a7fecdd7a5352300815
DESKTOP-KCAJR4O\ADMIRAL <shubham.butle@gmail.com>
2022-01-01 00:20:00 +0530

    additional files

---
+++ src/com/greatlearning/lab/driver/MergeSortImplementation.java
@@ -0,0 +1,81 @@
+package com.greatlearning.lab.driver;
+
+class MergeSortImplementation
+{
+    void merge(int arr[], int l, int m, int r)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+ 
+        int L[] = new int [n1];
+        int R[] = new int [n2];
+ 
+        for (int i=0; i<n1; ++i)
+            L[i] = arr[l + i];
+        for (int j=0; j<n2; ++j)
+            R[j] = arr[m + 1+ j];
+ 
+ 
+ 
+        int i = 0, j = 0;
+ 
+        int k = l;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+ 
+        /* Copy remaining elements of L[] if any */
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+ 
+        /* Copy remaining elements of R[] if any */
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+ 
+    // merge()
+    void sort(int[] notes, int left, int right)
+    {
+        if (left < right)
+        {
+            // Find the middle point
+            int mid = (left+right)/2;
+ 
+            // Sort first and second halves
+            sort(notes, left, mid);
+            sort(notes , mid+1, right);
+ 
+            // Merge the sorted halves
+            merge(notes, left, mid, right);
+        }
+    }
+ 
+    /* A utility function to print array of size n */
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+ 
+}
